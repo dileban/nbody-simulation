6ea6db58bd2abdc9d31827f03c02691e863f3413
Dileban Karunamoorthy <dilebank@au1.ibm.com>
2017-05-13 12:02:43 +1000

    Add comments to global constants

--- pnbody.c
+++ pnbody.c
@@ -14,21 +14,21 @@
 #include <mpi.h> 
 
 
-#define DEFAULT_N 10000   
-#define DEFAULT_TIME 1000 
-#define G 6.67300e-11     
-#define XBOUND 1.0e6      
-#define YBOUND 1.0e6      
-#define ZBOUND 1.0e6      
-#define RBOUND 10         
-#define DELTAT 0.01       
-#define THETA 1.0         
+#define DEFAULT_N 10000   // Number of particles                   
+#define DEFAULT_TIME 1000 // Number if iterations                  
+#define G 6.67300e-11     // Gravitational constant, m3 kg-1 s-2   
+#define XBOUND 1.0e6      // Width of space                        
+#define YBOUND 1.0e6      // Height of space                       
+#define ZBOUND 1.0e6      // Depth of space                        
+#define RBOUND 10         // Upper bound on radius                 
+#define DELTAT 0.01       // Time increament                       
+#define THETA 1.0         // Opening angle, for approximation in BH
 
 
-#define MASS_OF_JUPITER 1.899e27 
-#define MASS_OF_EARTH 5.974e24   
-#define MASS_OF_MOON 7.348e22    
-#define MASS_OF_UNKNOWN 1.899e12 
+#define MASS_OF_JUPITER 1.899e27  //  Sample masses for testing 
+#define MASS_OF_EARTH 5.974e24    //  and comparing the effect  
+#define MASS_OF_MOON 7.348e22     //  of using different values 
+#define MASS_OF_UNKNOWN 1.899e12  //  for m.                    
 
 
 
